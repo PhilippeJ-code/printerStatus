def8075bd092098c84c285b1731183082f77e85a
PhilippeJ-code <jacobs.philippe@skynet.be>
2021-03-08 21:29:48 +0100

    cron

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -22,45 +22,45 @@ require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 //
 function printerStatus_install() 
 {
-    config::save('functionality::cron::enable', 0, 'viessmann');
-    config::save('functionality::cron5::enable', 0, 'viessmann');
-    config::save('functionality::cron10::enable', 0, 'viessmann');
-    config::save('functionality::cron15::enable', 0, 'viessmann');
-    config::save('functionality::cron30::enable', 1, 'viessmann');
-    config::save('functionality::cronHourly::enable', 0, 'viessmann');
-    config::save('functionality::cronDaily::enable', 0, 'viessmann');
+    config::save('functionality::cron::enable', 0, 'printerStatus');
+    config::save('functionality::cron5::enable', 0, 'printerStatus');
+    config::save('functionality::cron10::enable', 0, 'printerStatus');
+    config::save('functionality::cron15::enable', 0, 'printerStatus');
+    config::save('functionality::cron30::enable', 1, 'printerStatus');
+    config::save('functionality::cronHourly::enable', 0, 'printerStatus');
+    config::save('functionality::cronDaily::enable', 0, 'printerStatus');
 }
 
 // Fonction exécutée automatiquement après la mise à jour du plugin
 //
 function printerStatus_update() 
 {
-    if (config::byKey('functionality::cron::enable', 'viessmann', -1) == -1) {
-        config::save('functionality::cron::enable', 0, 'viessmann');
+    if (config::byKey('functionality::cron::enable', 'printerStatus', -1) == -1) {
+        config::save('functionality::cron::enable', 0, 'printerStatus');
     }
 
-    if (config::byKey('functionality::cron5::enable', 'viessmann', -1) == -1) {
-        config::save('functionality::cron5::enable', 0, 'viessmann');
+    if (config::byKey('functionality::cron5::enable', 'printerStatus', -1) == -1) {
+        config::save('functionality::cron5::enable', 0, 'printerStatus');
     }
 
-    if (config::byKey('functionality::cron10::enable', 'viessmann', -1) == -1) {
-        config::save('functionality::cron10::enable', 0, 'viessmann');
+    if (config::byKey('functionality::cron10::enable', 'printerStatus', -1) == -1) {
+        config::save('functionality::cron10::enable', 0, 'printerStatus');
     }
 
-    if (config::byKey('functionality::cron15::enable', 'viessmann', -1) == -1) {
-        config::save('functionality::cron15::enable', 0, 'viessmann');
+    if (config::byKey('functionality::cron15::enable', 'printerStatus', -1) == -1) {
+        config::save('functionality::cron15::enable', 0, 'printerStatus');
     }
 
-    if (config::byKey('functionality::cron30::enable', 'viessmann', -1) == -1) {
-        config::save('functionality::cron30::enable', 1, 'viessmann');
+    if (config::byKey('functionality::cron30::enable', 'printerStatus', -1) == -1) {
+        config::save('functionality::cron30::enable', 1, 'printerStatus');
     }
 
-    if (config::byKey('functionality::cronHourly::enable', 'viessmann', -1) == -1) {
-        config::save('functionality::cronHourly::enable', 0, 'viessmann');
+    if (config::byKey('functionality::cronHourly::enable', 'printerStatus', -1) == -1) {
+        config::save('functionality::cronHourly::enable', 0, 'printerStatus');
     }
 
-    if (config::byKey('functionality::cronDaily::enable', 'viessmann', -1) == -1) {
-        config::save('functionality::cronDaily::enable', 0, 'viessmann');
+    if (config::byKey('functionality::cronDaily::enable', 'printerStatus', -1) == -1) {
+        config::save('functionality::cronDaily::enable', 0, 'printerStatus');
     }
 
     foreach (printerStatus::byType('printerStatus') as $printerStatus) {
